2639d5bfeb08add31d4776838e9f986cc00a6fbf
Carina Sweet <carina.sweet06@gmail.com>
2021-05-19 14:16:41 -0600

    Add age_in_days

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,10 +1,11 @@
 class Animal
-  attr_reader :kind, :weight, :age
+  attr_reader :kind, :weight, :age, :age_in_days
 
   def initialize(kind, weight, age)
     @kind = kind
     @weight = "#{weight} pounds"
     @age = "#{age} weeks"
+    @age_in_days = age * 7
   end
 
 end
