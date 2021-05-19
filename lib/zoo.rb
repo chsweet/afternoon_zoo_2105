9a9258524b0a166e5cb536a6e8fd600b03759c1a
Carina Sweet <carina.sweet06@gmail.com>
2021-05-19 14:56:35 -0600

    Add animal to inventory method

--- lib/zoo.rb
+++ lib/zoo.rb
@@ -1,2 +1,20 @@
 class Zoo
+  attr_reader :name, :street, :city, :state, :zip, :address, :inventory, :animal_count
+
+  def initialize(name, street, city, state, zip)
+    @name = name
+    @street = street
+    @city = city
+    @state = state
+    @zip = zip
+    # @address = "#{street}"
+    @inventory = []
+    @animal_count = @inventory.length
+  end
+
+  def add_animal(animal)
+    require "pry"; binding.pry
+    @inventory << animal
+  end
+
 end
--- spec/zoo_spec.rb
+++ spec/zoo_spec.rb
@@ -3,3 +3,63 @@ require './lib/animal'
 require './lib/zoo'
 
 RSpec.describe Zoo do
+  it 'exsits' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo).to be_an_instance_of(Zoo)
+  end
+
+  it 'has a street' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo.street).to eq("2300 Southern Blvd")
+  end
+
+  it 'has a city' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo.city).to eq("Bronx")
+  end
+
+  it 'has a state' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo.state).to eq("NY")
+  end
+
+  it 'has a zip' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo.zip).to eq("10460")
+  end
+
+  xit 'has an address' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo.address).to eq("2300 Southern Blvd Bronx, NY 10460")
+  end
+
+  it 'has an inventory' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo.inventory).to eq([])
+  end
+
+  it 'has an animal count' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+
+    expect(zoo.animal_count).to eq(0)
+  end
+
+  it 'adds animals to inventory' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+
+    zoo.add_animal(animal_1)
+    zoo.add_animal(animal_2)
+
+    expect(zoo.inventory).to eq([animal_1, animal_2])
+  end
+
+end
